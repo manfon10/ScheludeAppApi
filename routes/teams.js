f1c143addee06c9dcd421a66f9b06e8fb64b866d
Manuel Fonseca <manueldazafon@gmail.com>
2022-09-06 23:13:14 -0500

    Avances.

--- routes/teams.js
+++ routes/teams.js
@@ -10,30 +10,37 @@ const { protectToken } = authServices;
 
 const router = express.Router();
 
-router.get('/', catchAsync(async (req, res) => {
-  const sessionUser = await protectToken(req);
+router.get(
+  "/",
+  catchAsync(async (req, res) => {
+    const sessionUser = await protectToken(req);
 
-  console.log(sessionUser);
+    const teams = await getAll(sessionUser.user.data._id);
 
-  const teams = await getAll(sessionUser.user.data._id);
+    return res.status(teams.success ? 200 : 400).json(teams);
+  })
+);
 
-  return res.status(teams.success ? 200 : 400).json(teams)
-}));
+router.get(
+  "/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
 
-router.get('/:id', catchAsync(async (req, res) => {
-  const { id } = req.params;
+    const team = await getById(id);
 
-  const team = await getById(id);
+    return res.status(team.success ? 200 : 400).json(team);
+  })
+);
 
-  return res.status(team.success ? 200 : 400).json(team);
-}));
+router.post(
+  "/",
+  catchAsync(async (req, res) => {
+    const sessionUser = await protectToken(req);
 
-router.post('/', catchAsync(async (req, res) => {
-  const sessionUser = await protectToken(req);
+    const team = await create(req.body, sessionUser.user.data);
 
-  const team = await create(req.body, sessionUser.user.data);
-
-  return res.status(team.success ? 200 : 400).json(team);
-}));
+    return res.status(team.success ? 200 : 400).json(team);
+  })
+);
 
 module.exports = { teamsRouter: router };
--- services/teams.js
+++ services/teams.js
@@ -2,23 +2,25 @@ const Team = require("../models/Team");
 
 const teamsServices = {
   getAll: async (userId) => {
-    const teams = await Team.find({ userId: { $in: [userId] } }).populate("userId");
+    const teams = await Team.find({ users: { $in: [userId] } }).populate(
+      "users"
+    );
 
     return {
       data: teams,
       message: "Teams listed!",
       success: teams ? true : false,
-    }
+    };
   },
 
   getById: async (id) => {
-    const team = await Team.findById(id).populate("userId");
+    const team = await Team.findById(id).populate("users");
 
     return {
       data: team,
       message: "Team listed!",
       success: team ? true : false,
-    }
+    };
   },
 
   create: async (data, user) => {
@@ -31,9 +33,8 @@ const teamsServices = {
       data: newTeam,
       message: "Team created!",
       success: newTeam ? true : false,
-    }
+    };
   },
 };
 
 module.exports = teamsServices;
-
